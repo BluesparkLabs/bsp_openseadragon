76b52743d586e290af6701095e24b11a46ce83b5
James Wilson <jrwilson3@gmail.com>
2020-03-02 19:18:12 -0500

    UISS-189: Switch from Djatoka to IIIF-compatible image server
    In the process of switching to IIIF compatible server, we've also
    removed support for multiple resource URIs / Tile Sources due to the
    complexity around handling multiple asynchronous json requests to obtain
    the width/height definition of each image.
    
    We suspect that this feature was not being used at all in the first
    place.

--- js/bsp_openseadragon.js
+++ js/bsp_openseadragon.js
@@ -6,15 +6,36 @@
 
       var openSeadragonId = '#' + config['id'];
       $(openSeadragonId).each(function () {
-        if (!$(this).hasClass('processed')) {
-          config.tileSources = new Array();
-          resourceUri = (resourceUri instanceof Array) ? resourceUri : new Array(resourceUri);
-          $.each(resourceUri, function(index, uri) {
-            var tileSource = new OpenSeadragon.DjatokaTileSource(uri, settings.bspOpenSeadragon);
-            config.tileSources.push(tileSource);
+        var that = this;
+        if (!$(that).hasClass('processed')) {
+          if (resourceUri instanceof Array) {
+            console.error('Support for multiple tile sources has been deprecated.');
+            alert('Support for multiple tile sources has been deprecated. Please contact a site administrator.');
+            return;
+          }
+          jQuery.getJSON(resourceUri, function(data, textStatus, jqXHR) {
+            config.id = $(that).attr('id');
+            config.tileSources = new Array();
+            config.tileSources = [{
+              '@context': 'http://iiif.io/api/image/2/context.json',
+              '@id': resourceUri,
+              'height': data.height,
+              'width': data.width,
+              'profile': [ 'http://iiif.io/api/image/2/level2.json' ],
+              'protocol': 'http://iiif.io/api/image',
+              'tiles': [{
+                'scaleFactors': [ 1, 2, 4, 8, 16, 32 ],
+                'width': 1024
+              }]
+            }];
+            new OpenSeadragon(config);
+          })
+          .fail(function () {
+            console.error("Error getting the image resource for " + resourceUri);
+          })
+          .always(function() {
+            $(that).addClass('processed');
           });
-          new OpenSeadragon(config);
-          $(this).addClass('processed');
         }
       });
     }
